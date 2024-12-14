fabadd5d4f866ae4fdc55203b8db155b6fd7813b
Atri Dey <76494658+atridey@users.noreply.github.com>
2024-12-14 16:03:16 -0500

    I don't think we need that bit

--- backend/rest_server.py
+++ backend/rest_server.py
@@ -50,17 +50,9 @@ def get_summary(date):
 from typing import Union
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(redoc_url = None)
 router = APIRouter(prefix = '/api')
-
-origins = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://localhost:3000",  # React/Frontend origin
-    "http://127.0.0.1:3000"
-]
 
 @router.get('/get_dates/')
 def return_dates():
