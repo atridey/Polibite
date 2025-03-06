11e6920a812a261904aafbd7dd688ee50e6acd0b
Atri Dey <76494658+atridey@users.noreply.github.com>
2025-03-06 17:42:28 -0500

    Update rest_server.py

--- backend/rest_server.py
+++ backend/rest_server.py
@@ -4,14 +4,19 @@ import os
 import requests
 from io import BytesIO
 from pypdf import PdfReader
-import google.generativeai as genai
-
 CKEY = os.getenv('CKEY')
+
+import google.generativeai as genai
 GKEY = os.getenv('GKEY')
 genai.configure(api_key=GKEY)
 model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
-summary_cache = {}
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate(json.loads(FIREBASE_CREDENTIALS))
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 def get_recent_records():
     return requests.get(f'https://api.congress.gov/v3/daily-congressional-record?format=json&limit=10&api_key={CKEY}').json()['dailyCongressionalRecord']
@@ -21,20 +26,23 @@ def get_dates():
     dates = []
     for record in recent_records:
         dates.append(record['issueDate'][:10])
-    purge_cache(dates)
+    #purge_cache(dates)
     return dates
 
+#Not currently in use
 def purge_cache(dates):
-    global summary_cache
-    for cached_date in list(summary_cache.keys()):
-        if cached_date not in dates:
-            del summary_cache[cached_date]
+    cache = db.collection("summary_cache")
+    for entry in cache.stream():
+        date = list(entry.to_dict().keys())[0]
+        if date not in dates:
+            cache.document(date).delete()
 
 Q_PT1 = 'Within the triple curly braces is an entire day issue for a congressional session:\n{{{'
 Q_PT2 = '}}}\nConsidering a reader with political knowledge equal to or slightly less than the average American, summarize the issue in a concise, informative, impartial, and engaging way. Structure the response in a way that is appropriate for one single paragraph of text. Do not use Markdown, and use formatting tools such as Unicode bullets (do not add bullets to headings) or Unicode bold characters instead. Use paragraph breaks when necessary (with a blank space in between), and ensure that any and all line breaks are represented with HTML <br> tags (NOT \'\\n\') and not actual line breaks in the outputted string. Prioritize proceedings related to law with policy implications, such as bills and resolutions, and specifically list and name the most significant ones with bill numbers. Any non-legal things can be included if space remains. Limit the response to approximately 250-300 words. Begin your response with \"Here\'s a Polibite for you!\"'
 def get_summary(date):
-    if date in list(summary_cache.keys()):
-        return summary_cache[date]
+    cached = db.collection("summary_cache").document(date)
+    if cached.get().exists:
+        return cached.get().to_dict()['summary']
     else:
         recent_records = get_recent_records()
         for record in recent_records:
@@ -55,8 +63,9 @@ def get_summary(date):
         for page in reader.pages:
             full_text += page.extract_text()
 
-        return model.generate_content(Q_PT1 + full_text + Q_PT2).text
-
+        summary = model.generate_content(Q_PT1 + full_text + Q_PT2).text
+        cached.set({'summary': summary})
+        return summary
 
 #################### BACKEND REST API #########################
 
@@ -68,17 +77,13 @@ app = FastAPI(redoc_url=None)
 router = APIRouter(prefix='/api')
 
 @router.get('/get_dates/')
-def return_dates(background_tasks: BackgroundTasks):
+def return_dates():
     dates = get_dates()
     return {'date_list': dates}
 
 @router.get('/get_summary/{ISOSTRING}')
 def return_summary(ISOSTRING: str):
-    if ISOSTRING in summary_cache:
-        summary = summary_cache[ISOSTRING]
-    else:
-        summary = get_summary(ISOSTRING)
-        summary_cache[ISOSTRING] = summary  # Cache it for future use
+    summary = get_summary(ISOSTRING)
     return {'summary': summary}
 
 app.include_router(router)
